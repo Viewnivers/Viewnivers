0f58e142db46069e1a2eeb1adcada730aea35c44
limzodisc <limzodisc@gmail.com>
2025-06-13 08:32:53 +0000

    V1.5.2.7

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
 
     // Read user data from users.js
-    $users_js_content = file_get_contents('/mnt/data/users.js');
+    $users_js_content = file_get_contents('users.js');
 
     // Extract the array part from the JS file content
     $users_array_string = preg_replace('/^\s*export\s+const\s+users\s*=\s*/', '', $users_js_content);
